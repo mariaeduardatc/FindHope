3b4a436004914ab6c3dd906ec9e8e7bfec6cd987
Libby Greenlaw <33658445+lgreenlaw2022@users.noreply.github.com>
2023-03-25 18:50:08 +0530

    What you'll get section, completed with text

--- src/components/Get/index.tsx
+++ src/components/Get/index.tsx
@@ -1,13 +1,132 @@
-import { Container, Title } from "./styles";
+import { Gets, Container, Description, Segment, Day, Title } from "./styles";
 
-export function Get(){
+export function Get(){ 
     return(
         <Container>
             <Title>
                 <h1>What you'll get...</h1>
                 <h3>3 modules in 3 weeks</h3>
             </Title>
+            <Gets>
+                <Segment>
+                    <h3>Relationships</h3>
+                </Segment>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 1:</Day>
+                    <h4>Dealing with attachment issues</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 2:</Day>
+                    <h4>Ending or healing a toxic relationship</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 3:</Day>
+                    <h4>Dealing with infidelity</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 4:</Day>
+                    <h4>Dealing with a dysfunctional family</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 5:</Day>
+                    <h4>Getting over breakup/divorce</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 6:</Day>
+                    <h4>Building boundaries</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 7:</Day>
+                    <h4>Build a healthy relationship</h4>
+                </Description>
 
+
+                <Segment>
+                    <h3>Anxiety</h3>
+                </Segment>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 8:</Day>
+                    <h4>Finding your triggers</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 9:</Day>
+                    <h4>Dealing with panic attacks</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 10:</Day>
+                    <h4>Helping a partner with anxiety</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 11:</Day>
+                    <h4>Identifying catastrophized thoughts</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 12:</Day>
+                    <h4>Breathwork for mindfulness</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 13:</Day>
+                    <h4>Yoga for anxiety & stress</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 14:</Day>
+                    <h4>Finding your own yoga style</h4>
+                </Description>
+
+                <Segment>
+                    <h3>Trauma</h3>
+                </Segment>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 15:</Day>
+                    <h4>Recognising trauma responses</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 16:</Day>
+                    <h4>Dealing with childhood trauma</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 17:</Day>
+                    <h4>Healing your inner child</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 18:</Day>
+                    <h4>Helping a partner deal with trauma</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 19:</Day>
+                    <h4>Dealing with grief</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 20:</Day>
+                    <h4>Dealing with generational trauma</h4>
+                </Description>
+                <Description>
+                    <img src="" alt="" />
+                    <Day>Day 21:</Day>
+                    <h4>Creating your own self love tools</h4>
+                </Description>
+                
+            </Gets>
 
         </Container>
     )
--- src/components/Get/styles.tsx
+++ src/components/Get/styles.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 
-
 export const Container = styled.div`
     text-align: center;
     background-color: var(--white);
@@ -15,5 +14,59 @@ export const Title = styled.div`
 
     h3{
         font-weight: 200;
+    }
+`
+export const Gets = styled.div` //TODO: naming standard, this is weird here
+    padding: 0rem 2rem;
+`
+export const Segment = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 1rem;
+    margin-top: 2rem;
+    justify-content: center;
+
+    h3{
+        font-size: 20px;
+        font-weight: 400;
+        text-decoration: underline;
+        align-items: center;
+    }
+
+`
+export const Day = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 0.7rem;
+    margin-right: 0.3rem;
+
+    h4{
+        font-size: 18px;
+        font-weight: 100;
+        text-decoration: bold;
+    }
+
+`
+export const Description = styled.div`
+     
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 0.3rem;
+    justify-content: left;    
+    align-items: center;
+
+    img{ 
+        padding: 15px;
+        width: 1rem;    //TODO: these lines are not doing anything
+        height: 1rem;
+        background-color: rgba(60, 85, 86, 0.5);
+        margin-bottom: 0rem;
+        margin-left: 25%;
+    }
+
+    h4{
+        font-size: 18px;
+        font-weight: 100;
+        display: flex;
     }
 `
